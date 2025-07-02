51cca85a577ccc060f4d6974c22c8d298c31eceb
Ilham El Gharbi <129683687+Ilhamelgharbi@users.noreply.github.com>
2025-07-02 16:48:03 +0100

    Update exo1.py

--- challenge2/exo1.py
+++ challenge2/exo1.py
@@ -11,8 +11,5 @@ name= input("enter your name : ")
 salary= float(input("enter your hourly salary : "))
 hours= float(input("enter the number of the hour that you worked : "))
 totalsalary = horaire_supp(hours, salary, name)
-<<<<<<< HEAD
 print(f" {name} Your total pay is: {totalsalary} dhs")
-=======
-print(f" {name} Your total pay is: {totalsalary} dhs")
->>>>>>> 0f492872d0a2538dd919989f34e9ec2af7f80ed5
+
