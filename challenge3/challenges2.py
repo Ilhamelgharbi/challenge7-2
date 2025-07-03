5d1ec63e184cccaa80124f74a36ad2d0c882c44d
Ilham El Gharbi <129683687+Ilhamelgharbi@users.noreply.github.com>
2025-07-03 10:03:40 +0100

    Update challenges2.py

--- challenge3/challenges2.py
+++ challenge3/challenges2.py
@@ -3,8 +3,8 @@ ch2 = input("Enter the second string: ")
 var1 =ch1.split()
 var2 =ch2.split()
 var3 = []
-for i in range(len(var1)):
-    for j in range(len(var2)):
+for i in range(len(var1)-1):
+    for j in range(len(var2)-1):
         if var1[i] == var2[j]:
             var3.append(var1[i])
 print("Common words found:", var3)
